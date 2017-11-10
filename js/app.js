b1b700a447569aa6bda279f47f83f4a3f4225e11
lindskelsey <lindsaykelsey@gmail.com>
2017-11-09 20:46:44 -0600

    randomized tilesets

--- js/app.js
+++ js/app.js
@@ -2,43 +2,83 @@
 //***************Build Randomly Generated Map***********//
 //******************************************************//
 
+var tilesetTotal = 3;
+var tilesTotal = 2;
+var tilesetNumber = randomNumber(1, tilesetTotal);
 
-//*********Function to Randomize Array with numbers 1-2*******//
+//*************Random Number Generator*************//
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//***********Function to Randomize Array*******//
 
 function createRandomArray(width, height) {
-    var result = [];
-    for (var i = 0; i < width; i++) {
-        result[i] = [];
-        for (var j = 0; j < height; j++) {
-            result[i][j] = (Math.floor(Math.random() * 2) + 1);
-        }
+  var result = [];
+  for (var i = 0; i < width; i++) {
+    result[i] = [];
+    for (var j = 0; j < height; j++) {
+      result[i][j] = Math.floor(Math.random() * 2) + 1;
     }
-    return result;
+  }
+  return result;
 }
+
+//*********Function to Randomize Array with Images*******//
 
 var generatedArray = createRandomArray(5, 5)
 
-//******************Draw Map from Random Array************//
+//**************Function to dynamically create CSS classes*****//
 
+function createClass(name,properties){
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    if(!(style.sheet||{}).insertRule)
+        (style.styleSheet || style.sheet).addRule(name, rules);
+    else
+        style.sheet.insertRule(name+"{"+properties+"}",0);
+}
+
+
+//****************Function************//
+
+function createTileClasses() {
+  for (i = 1; i <= tilesTotal; i++) {
+    var divClass = (".tile" + i);
+    var newProperty = ("background-image: url(\"images/tiles/" + tilesetNumber + "/tile" + i + ".png\");")
+    createClass(divClass, newProperty);
+    console.log(i);
+    console.log(divClass);
+    console.log(newProperty);
+  }
+}
+
+
+//******************Draw Map************//
 
 function drawMap() {
     var container = document.getElementById("container");
     for (var i = 0; i < generatedArray.length; i++) {
         for (var j = 0; j < generatedArray[i].length; j++) {
             if (parseInt(generatedArray[i][j]) == 1) {
-                container.innerHTML += '<div class="tile1"></div>';
+                container.innerHTML += '<div class="tile tile1"></div>';
                 console.log("tile1");
             } else if (parseInt(generatedArray[i][j]) == 2) {
-                container.innerHTML += '<div class="tile2"></div>';
+                container.innerHTML += '<div class="tile tile2"></div>';
                 console.log("tile2");
             }
         }
     }
 }
 
+
 //**********create map on load*********//
 
+
 window.onload = function() {
+    createTileClasses();
     drawMap();
 }
 
